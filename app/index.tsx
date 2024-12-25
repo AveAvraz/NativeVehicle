7c5f7b0da96dc697860932d0efe3f636e71ebc41
Avraz <avraz@Avrazs-MacBook-Pro.local>
2024-12-25 16:46:56 +0700

    installed gluestack

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
 import { Text, View,FlatList } from "react-native";
 import products from '../assets/products.json'
  import ProductListItem from "@/components/productListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 
 
 export default function Index() {
   return (
+
+
+    <Button>
+      <ButtonText>Press</ButtonText>
+    </Button>
     
-      <FlatList 
+      /*<FlatList 
       data={products}
       renderItem={({item}) => <ProductListItem product={item}/>}
-      />
+      /> */
      
   );
 }
